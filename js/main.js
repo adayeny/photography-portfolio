ef2f83469d6cb6a896ffa422298c755cd0249c8f
adayeny <adayeny@avmoda.tech>
2026-09-01 15:56:17 -0700

    Minor adjustments

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Avesta Dayeny.", "an Engineer.", "a Mathematician.", "Freelancera Photographer.", "Freelancer."],
       typeSpeed: 100,
       loop: true,
     });
